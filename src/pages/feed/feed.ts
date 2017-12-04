1e27acc58b1b4a2d14721729992b46581bd73502
Raul Silva Sousa <sraul.tec2000@gmail.com>
2017-12-04 16:00:30 -0300

    Subindo o projeto

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+  public name_feed:string = "Vitor Marques";
+  public date_feed:string = "9 de novembro as 10:30";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FeedPage');
+  }
+
+}
